afc84a4fe00bafca393af3fb3c77882b562634ec
f0xyyyk1ddd <scvdldj@icloud.com>
2026-07-02 12:01:21 +0500

    Update VPNManager.swift

--- Sources/VPNManager.swift
+++ Sources/VPNManager.swift
@@ -117,8 +117,8 @@ class VPNManager {
             
             let corePath = Bundle.main.path(forResource: "sing-box", ofType: nil) ?? "/usr/local/bin/sing-box"
             
-            // Снимаем карантин, даем права на исполнение и запускаем в фоне, перенаправляя логи
-            let script = "do shell script \"xattr -cr '\(corePath)'; chmod +x '\(corePath)'; '\(corePath)' run -c '\(configPath)' > /tmp/sing-box.log 2>&1 &\" with administrator privileges"
+            // Снимаем карантин, даем права на исполнение и запускаем через nohup отвязанным от shell-сессии
+            let script = "do shell script \"xattr -cr '\(corePath)'; chmod +x '\(corePath)'; nohup '\(corePath)' run -c '\(configPath)' > /tmp/sing-box.log 2>&1 </dev/null &\" with administrator privileges"
             
             DispatchQueue.global(qos: .background).async {
                 var error: NSDictionary?
